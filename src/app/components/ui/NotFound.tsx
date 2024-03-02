678da6a35aabeb12d7690b4af676957d036161bd
Simone <150595274+simonetimi@users.noreply.github.com>
2024-03-02 22:53:06 +0100

    fix: fix alignment of NotFound component

--- src/app/components/ui/NotFound.tsx
+++ src/app/components/ui/NotFound.tsx
@@ -8,7 +8,7 @@ export default function NotFound({
   redirectUrl: string;
 }) {
   return (
-    <main className="flex h-screen flex-col items-center justify-center gap-4">
+    <main className="flex h-96 flex-col items-center justify-center gap-4">
       <h2 className="text-xl font-semibold">404 Not Found</h2>
       <p>{errorMessage}</p>
       <Link
