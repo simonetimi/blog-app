9bf71017dbeaccd863039302a2d2e047fc3ec855
Simone <150595274+simonetimi@users.noreply.github.com>
2024-03-02 15:10:57 +0100

    fix: fix bug on get post route that would only populate the comments array with the IDs

--- src/app/api/posts/get/route.ts
+++ src/app/api/posts/get/route.ts
@@ -2,14 +2,17 @@ import { jwtVerify } from 'jose';
 import { NextRequest, NextResponse } from 'next/server';
 
 import { connect } from '@/db/db-config';
+import Comment from '@/models/comment';
 import Post from '@/models/post';
 import User from '@/models/user';
 
 export async function POST(request: NextRequest) {
   try {
     const session = request.cookies.get('session')?.value || '';
-    const secret = new TextEncoder().encode(process.env.TOKEN_SECRET!);
-    const { payload } = await jwtVerify(session, secret);
+    let isSession = false;
+    if (session !== '') {
+      isSession = true;
+    }
 
     await connect();
     const reqBody = await request.json();
@@ -22,23 +25,31 @@ export async function POST(request: NextRequest) {
       .populate({
         path: 'comments',
         select: 'author content publishDate',
+        model: Comment,
         populate: { path: 'author', select: 'username', model: User },
       })
       .exec();
-
-    // if the user isn't the post author and the post is a draft, return an error (only the author should be able to see the draft)
-    if (post.author !== payload.username && post.isDraft === true) {
-      return NextResponse.json(
-        { error: 'User is not authorized' },
-        { status: 403 },
-      );
-    }
 
     const response = NextResponse.json({
       message: 'Post has been fetched',
       success: true,
       post: post,
+      isSession: isSession,
     });
+
+    if (session === '' && !post.isDraft) {
+      return response;
+    }
+
+    const secret = new TextEncoder().encode(process.env.TOKEN_SECRET!);
+    const { payload } = await jwtVerify(session, secret);
+    // if the user isn't the post author and the post is a draft, return an error (only the author should be able to see the draft)
+    if (post.author !== payload.username && post.isDraft) {
+      return NextResponse.json(
+        { error: 'User is not authorized' },
+        { status: 403 },
+      );
+    }
 
     return response;
   } catch (error) {
