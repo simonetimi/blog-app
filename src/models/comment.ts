6c13217a9adb9515f43a1885a267360d7ee7be54
Simone <150595274+simonetimi@users.noreply.github.com>
2024-03-02 10:49:52 +0100

    fix: fix bug on comment model incorrectly referencing to post

--- src/models/comment.ts
+++ src/models/comment.ts
@@ -5,17 +5,18 @@ import type { UserInt } from './user';
 
 export interface CommentInt extends Document {
   content: string;
-  _author: UserInt['_id'];
+  author: UserInt['_id'];
   publishDate: Date;
 }
 
 const commentSchema = new mongoose.Schema({
   content: { type: String, required: [true, 'Content is required'] },
-  _author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   publishDate: { type: Date },
 });
 
 const Comment =
-  mongoose.models.Post || mongoose.model<CommentInt>('Post', commentSchema);
+  mongoose.models.Comment ||
+  mongoose.model<CommentInt>('Comment', commentSchema);
 
 export default Comment;
