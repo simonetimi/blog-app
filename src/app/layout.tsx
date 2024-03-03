3d86341526a3da6d97479f3eef8cdf6b72d429b9
Simone <150595274+simonetimi@users.noreply.github.com>
2024-03-03 10:53:36 +0100

    feat: add icons in metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,22 @@ const recursive = Recursive({
 export const metadata: Metadata = {
   title: 'Inkwell Insights - Blog App',
   description: 'Fullstack Blog App project',
+  icons: [
+    { rel: 'icon', url: '/favicon.ico', type: 'image/x-icon' },
+    {
+      rel: 'icon',
+      url: '/android-chrome-192x192.png',
+      sizes: '192x192',
+      type: 'image/png',
+    },
+    {
+      rel: 'icon',
+      url: '/android-chrome-512x512.png',
+      sizes: '512x512',
+      type: 'image/png',
+    },
+    { rel: 'apple-touch-icon', url: '/apple-touch-icon.png' },
+  ],
 };
 
 export default async function RootLayout({
